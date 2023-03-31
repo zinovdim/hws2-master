e73f744c95fb2acd22f71343f7c7333d62504204
zinovdim <zinovdim@yandex.ru>
2023-03-31 22:07:51 +0300

    Change Sidebar.tsx

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className= {({isActive}) => (isActive ? s.open : '')}
+                        className= {({isActive}) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Pre-junior
@@ -40,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className= {({isActive}) => (isActive ? s.open : '')}
+                        className= {({isActive}) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior
@@ -49,7 +49,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className= {({isActive}) => (isActive ? s.open : '')}
+                        className= {({isActive}) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior Plus
