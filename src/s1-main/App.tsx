821faabeecbc88aa45af8ac691314e1b59c17247
zinovdim <zinovdim@yandex.ru>
2023-04-11 19:53:39 +0300

    Change HW6.tsx

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,7 +5,8 @@ import HW2 from "../s2-homeworks/hw02/HW2";
 import HW3 from "../s2-homeworks/hw03/HW3";
 import HW4 from "../s2-homeworks/hw04/HW4";
 import HW5 from '../s2-homeworks/hw05/HW5';
-import Junior from "../s2-homeworks/hw05/pages/Junior";
+import HW6 from '../s2-homeworks/hw06/HW6';
+
 
 
 
@@ -19,7 +20,7 @@ function App() {
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            <Junior/>
+            <HW6/>
 
 
 
